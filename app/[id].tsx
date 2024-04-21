15b52cce1bc5a4ba53d6e556aa0a165ae3f1ed54
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-04-21 21:19:59 +0530

    ✨ Add: details page for every city

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,21 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import { Image } from 'expo-image';
+import cities from '../assets/data/Cities.json';
 
-const DetailsPage = () => {
+export default function DynamicPage() {
+  const { id } = useLocalSearchParams();
+
+  const data = cities.find((city) => city.id === Number(id));
+
   return (
     <View>
-      <Text>[id]</Text>
+      <Image source={data?.image_source} className=" w-full" style={{ height: 500 }} />
+      <View className="mt-[40px] gap-y-[24px] px-[24px]">
+        <Text className="text-5xl font-bold">{data?.city_name}</Text>
+        <Text className="text-lg">{data?.description}</Text>
+      </View>
     </View>
   );
-};
-
-export default DetailsPage;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,33 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Pressable } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Image } from 'expo-image';
 import cities from '../assets/data/Cities.json';
-import { BlurView } from 'expo-blur';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
 
-const CityCard = ({ source, name }: { source: string; name: string }) => {
+const CityCard = ({ source, name, id }: { source: string; name: string; id: number }) => {
+  const router = useRouter();
+
+  const handlePress = (id: number) => {
+    router.push({ pathname: '/[id]', params: { id: id } });
+  };
+
   return (
-    <View className="mb-10 w-full">
-      <View className={styles.cardImageContainer}>
-        <Image className={styles.cardImage} style={{ height: 300 }} source={`${source}`} />
-      </View>
-      <View className={styles.cardTextContainer}>
-        <View
-          className="overflow-hidden rounded-xl"
-          style={{ borderWidth: 1, borderColor: 'rgba(255, 255, 255, 0.7)' }}>
-          <BlurView
-            className="h-full w-full px-6 py-8"
-            experimentalBlurMethod="dimezisBlurView"
-            tint="light"
-            intensity={50}>
-            <Text className={styles.cardText}>{name}</Text>
-          </BlurView>
+    <Pressable onPress={() => handlePress(id)}>
+      <View className="mb-10 w-full">
+        <View className={styles.cardImageContainer}>
+          <Image className={styles.cardImage} style={{ height: 300 }} source={`${source}`} />
         </View>
+        <LinearGradient
+          className={styles.cardTextContainer}
+          colors={['rgba(255, 255, 255, 0)', 'rgba(0, 0, 0, 0.5)']}>
+          <View className="overflow-hidden rounded-xl">
+            <Text className={styles.cardText}>{name}</Text>
+          </View>
+        </LinearGradient>
       </View>
-    </View>
+    </Pressable>
   );
 };
 
@@ -36,19 +39,20 @@ const index = () => {
           data={cities}
           showsVerticalScrollIndicator={false}
           renderItem={({ item }) => (
-            <CityCard name={item.city_name} source={item.image_source} key={item.id} />
-          )}></FlatList>
+            <CityCard id={item.id} name={item.city_name} source={item.image_source} key={item.id} />
+          )}
+        />
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = {
-  screen: 'px-[24px] py-[24px] h-full',
+  screen: 'px-[24px] pt-[24px] h-full',
   cardImage: 'w-full object-cover rounded-lg',
-  cardImageContainer: 'rounded-2xl overflow-hidden mb-4',
+  cardImageContainer: 'rounded-2xl overflow-hidden',
   cardText: 'text-4xl font-semibold text-white',
-  cardTextContainer: 'w-full absolute bottom-0 overflow-hidden rounded-b-2xl px-6 py-12',
+  cardTextContainer: 'w-full absolute bottom-0 overflow-hidden rounded-b-2xl px-6 py-6',
 };
 
 export default index;
