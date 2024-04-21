5d4a7011eac33d353800865c7b91d3449004d0b8
Sourabh Kumar Singh <sour.bt21cs56@opju.ac.in>
2024-04-21 17:28:44 +0530

    🌱 Seed: Dummy City Data with Image and Name

---
+++ app/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const DetailsPage = () => {
+  return (
+    <View>
+      <Text>[id]</Text>
+    </View>
+  );
+};
+
+export default DetailsPage;
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,54 @@
-import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image } from 'expo-image';
 import cities from '../assets/data/Cities.json';
+import { BlurView } from 'expo-blur';
 
-const index = () => {
+const CityCard = ({ source, name }: { source: string; name: string }) => {
   return (
-    <View className={styles.screen} style={{ marginTop: StatusBar.currentHeight }}>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos molestiae neque dolorem
-        sunt pariatur sint, cum eligendi voluptatum quaerat est aliquid inventore accusamus soluta
-        veritatis voluptate praesentium tenetur, ipsam delectus eum! Aliquam exercitationem
-        cupiditate fugit ut voluptates laborum aliquid minima tempora animi. Temporibus quia natus
-        optio sunt quaerat accusamus ullam?
-      </Text>
+    <View className="mb-10 w-full">
+      <View className={styles.cardImageContainer}>
+        <Image className={styles.cardImage} style={{ height: 300 }} source={`${source}`} />
+      </View>
+      <View className={styles.cardTextContainer}>
+        <View
+          className="overflow-hidden rounded-xl"
+          style={{ borderWidth: 1, borderColor: 'rgba(255, 255, 255, 0.7)' }}>
+          <BlurView
+            className="h-full w-full px-6 py-8"
+            experimentalBlurMethod="dimezisBlurView"
+            tint="light"
+            intensity={50}>
+            <Text className={styles.cardText}>{name}</Text>
+          </BlurView>
+        </View>
+      </View>
     </View>
   );
 };
 
+const index = () => {
+  return (
+    <SafeAreaView>
+      <View className={styles.screen}>
+        <FlatList
+          data={cities}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <CityCard name={item.city_name} source={item.image_source} key={item.id} />
+          )}></FlatList>
+      </View>
+    </SafeAreaView>
+  );
+};
+
 const styles = {
-  screen: 'h-full w-full justify-center items-center flex-1',
+  screen: 'px-[24px] py-[24px] h-full',
+  cardImage: 'w-full object-cover rounded-lg',
+  cardImageContainer: 'rounded-2xl overflow-hidden mb-4',
+  cardText: 'text-4xl font-semibold text-white',
+  cardTextContainer: 'w-full absolute bottom-0 overflow-hidden rounded-b-2xl px-6 py-12',
 };
 
 export default index;
